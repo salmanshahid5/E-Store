c89923dd81d08f07f6ebec8946400be8759f51f6
salmanshahid5 <salmanshahid0333@gmail.com>
2024-07-29 20:26:51 +0500

    display categories

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ let typingEffect = new Typed(".typedText", {
     backSpeed: 80,
     backDelay: 2000
 })
-
+const loader = document.getElementById('loader');
 document.addEventListener('DOMContentLoaded', function () {
-    const loader = document.getElementById('loader');
     loader.style.display = 'block'
     fetch('https://fakestoreapi.com/products')
         .then(res => res.json())
@@ -25,7 +24,127 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         })
         .catch(error => console.log('Error fetching products:', error))
-        .finally(()=>{
+        .finally(() => {
             loader.style.display = 'none'
         });
+});
+
+// for electronics
+
+document.querySelector('#electronics').addEventListener('click', async () => {
+    try {
+        loader.style.display = 'block';
+        const electronics = fetch('https://fakestoreapi.com/products/category/electronics');
+        const electronicsData = await electronics;
+        if (!electronicsData.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const electronicsDataJson = await electronicsData.json();
+        console.log(electronicsDataJson);
+        const products = document.querySelector('.product-grid');
+        products.innerHTML = "";
+        electronicsDataJson.forEach((element) => {
+            const productItems = document.createElement('div');
+            productItems.className = 'product-item';
+            productItems.innerHTML = `<img src="${element.image}" alt="Product 1">
+                <p>${element.title}</p>
+                <p>$${element.price}</p>`
+            products.appendChild(productItems)
+        });
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+    finally {
+        loader.style.display = 'none';
+    }
+});
+
+// for mens
+
+document.querySelector('#mens').addEventListener('click', async () => {
+    try {
+        loader.style.display = 'block';
+        const mens = fetch('https://fakestoreapi.com/products/category/men%27s%20clothing')
+        const mensData = await mens;
+        if (!mensData.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const mensDataJson = await mensData.json();
+        console.log(mensData);
+        const products = document.querySelector('.product-grid');
+        products.innerHTML = "";
+        mensDataJson.forEach((element) => {
+            const productItems = document.createElement('div');
+            productItems.className = 'product-item';
+            productItems.innerHTML = `<img src="${element.image}" alt="Product 1">
+                <p>${element.title}</p>
+                <p>$${element.price}</p>`
+            products.appendChild(productItems)
+        });
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+    finally {
+        loader.style.display = 'none';
+    }
+});
+
+// for women
+
+document.querySelector('#women').addEventListener('click', async () => {
+    try {
+        loader.style.display = 'block';
+        const women = fetch('https://fakestoreapi.com/products/category/women%27s%20clothing')
+        const womenData = await women;
+        if (!womenData.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const womenDataJson = await womenData.json();
+        console.log(womenDataJson);
+        const products = document.querySelector('.product-grid');
+        products.innerHTML = "";
+        womenDataJson.forEach((element) => {
+            const productItems = document.createElement('div');
+            productItems.className = 'product-item';
+            productItems.innerHTML = `<img src="${element.image}" alt="Product 1">
+                <p>${element.title}</p>
+                <p>$${element.price}</p>`
+            products.appendChild(productItems)
+        });
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+    finally {
+        loader.style.display = 'none';
+    }
+});
+
+// for Jewelery
+
+document.querySelector('#Jewelery').addEventListener('click', async () => {
+    try {
+        loader.style.display = 'block';
+        const Jewelery = fetch('https://fakestoreapi.com/products/category/jewelery')
+        const JeweleryData = await Jewelery;
+        if (!JeweleryData.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const JeweleryDataJson = await JeweleryData.json();
+        console.log(JeweleryDataJson);
+        const products = document.querySelector('.product-grid');
+        products.innerHTML = "";
+        JeweleryDataJson.forEach((element) => {
+            const productItems = document.createElement('div');
+            productItems.className = 'product-item';
+            productItems.innerHTML = `<img src="${element.image}" alt="Product 1">
+                <p>${element.title}</p>
+                <p>$${element.price}</p>`
+            products.appendChild(productItems)
+        });
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+    finally {
+        loader.style.display = 'none';
+    }
 });
